a2817fa90338dd6cc9d65ce963b9b9e9b71cfff0
Zaikron <anthony.sandoval1805@gmail.com>
2021-09-23 09:52:28 -0500

    Creacion del proyecto de netbeans

---
+++ JavaProgram/SolucionesPT/src/main/java/com/mycompany/solucionespt/Main.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.solucionespt;
+
+/**
+ *
+ * @author AnthonySandoval
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+        mf.setLocationRelativeTo(null);
+    }
+}
